00c6159a70816ca5cf52738457050b0c4228da86
kiki44444 <116151422+kiki44444@users.noreply.github.com>
2025-12-09 15:31:58 +0100

    [Silver V] Title: 숫자 카드, Time: 216 ms, Memory: 4168 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M, num;
+    cin >> N;
+    int* arr = new int[N];
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    cin >> M;
+    for(int i = 0; i < M; i++){
+        cin >> num;
+        if(binary_search(arr, arr + N, num)){
+            cout << '1' << ' ';
+        }
+        else{
+            cout << '0' << ' ';
+        }
+    }
+}
