1e49c51819f5ddc6903b810bbec50aafa7d33f71
kiki44444 <116151422+kiki44444@users.noreply.github.com>
2025-12-15 22:57:29 +0100

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+long long fibo(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << fibo(N);
+}
